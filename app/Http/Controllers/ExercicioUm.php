8cee2f272e2424bbdb602f85ffa012b39dc4b8a0
MariaLuizaAdame33 <maluzinha.adame@gmail.com>
2023-08-01 10:08:51 -0300

    atualização exercicio um

--- app/Http/Controllers/ExercicioUm.php
+++ app/Http/Controllers/ExercicioUm.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ExercicioUm extends Controller
 {
-    //
+    public function multiplicar(Request $request){
+        $primeiraEntrada = $request->primeiro_numero;
+        $segundaEntrada = $request->segundo_numero;  
+
+        return json_encode([
+            'resultado' => $primeiraEntrada * $segundaEntrada
+        ]);
+    }
 }
